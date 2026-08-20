212ce17f21b30ea66151b8d93b9686faba24eb68
Samuel Heluany <samuelheluany@hotmail.com>
2026-08-20 15:38:31 -0300

    refactor: create data-access to getTasks and add server-only

--- app/_data-access/get-tasks.ts
+++ app/_data-access/get-tasks.ts
@@ -1,8 +1,8 @@
-"use server";
-
 import { db } from "@/app/_lib/prisma";
+import { Tasks } from "../generated/prisma";
+import "server-only";
 
-export const getTasks = async () => {
+export const getTasks = async (): Promise<Tasks[]> => {
   try {
     const tasks = await db.tasks.findMany({});
     return tasks;
