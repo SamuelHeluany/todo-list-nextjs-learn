a2cae21c9b998123634d32f0273f4386d7bf6804
Samuel Heluany <samuelheluany@hotmail.com>
2026-08-27 15:45:05 -0300

    refactor: show empty state message when no tasks exists

--- app/_components/tasks.tsx
+++ app/_components/tasks.tsx
@@ -7,25 +7,30 @@ const TaskList = async () => {
   const tasks = await getTasks();
   return (
     <div className="mt-4 border-b">
-      {tasks.map((task) => (
-        <div
-          className="h-14 flex justify-between items-center border-t"
-          key={task.id}
-        >
+      {tasks.length === 0 ? (
+        <p className="text-sm py-4 border-t w-full text-center">
+          Você não possui tarefas cadastradas.
+        </p>
+      ) : (
+        tasks.map((task) => (
           <div
-            className={`${
-              task.done ? "w-1 h-full bg-green-400" : "w-1 h-full bg-red-400"
-            }`}
-          ></div>
-
-          <p className="flex-1 px-2 text-base">{task.task}</p>
-          <div className="flex items-center ">
-            <EditTask task={task} />
-            <DeleteTask id={task.id} />
-            <ToggleTaskButton id={task.id} done={task.done} />
+            className="h-14 flex justify-between items-center border-t"
+            key={task.id}
+          >
+            <div
+              className={`${
+                task.done ? "w-1 h-full bg-green-400" : "w-1 h-full bg-red-400"
+              }`}
+            ></div>
+            <p className="flex-1 px-2 text-base">{task.task}</p>
+            <div className="flex items-center ">
+              <EditTask task={task} />
+              <DeleteTask id={task.id} />
+              <ToggleTaskButton id={task.id} done={task.done} />
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
